1c045dad31c48772a5064bbc1b7c2117e99b8f65
Georgy Khudiakov <regesh@ya.ru>
2020-08-19 01:59:40 +0300

    Update typeFilter.js

--- typeFilter.js
+++ typeFilter.js
@@ -1,16 +1,23 @@
 "use strict";
+
 /*
-  1. Add class "fItem" to elements to be sorted with data-ftype attribute with sort value
-  2. Add class "fType" to elements to be button for selected filter with data-ftype attribute value for what to be sorted
-  3. Init class as new TypeFilter('.fItem','.fType') after event DOMContentLoaded where first param is class for items and second is for switchers
-  4. This classes can be changed on your wish.
+1. Add class "fItem" to elements to be sorted with data-ftype attribute with sort value
+2. Add class "fType" to elements to be button for selected filter with data-ftype attribute value for what to be sorted
+3. Init class as new TypeFilter('.fItem','.fType') after event DOMContentLoaded where first param is class for items and second is for switchers
+4. This classes can be changed on your wish
 */
 class TypeFilter {
   init() {
+    document.querySelector(self.buttons).classList.add('active');
     let self = this;
     document.querySelectorAll(self.buttons).forEach(x => {
       x.addEventListener('click', () => {
+        document.querySelectorAll(self.buttons).forEach(e => {
+          e.classList.remove('active');
+        });
+        x.classList.add('active');
         let type = x.getAttribute('data-ftype');
+
         if (type > 0) {
           document.querySelectorAll(self.items + '[data-ftype]').forEach(f => {
             if (f.getAttribute('data-ftype') != type) {
@@ -27,17 +34,20 @@ class TypeFilter {
       });
     });
   }
+
   constructor(items, buttons) {
     this.items = items;
     this.buttons = buttons;
+
     if (document.querySelector(this.items) && document.querySelector(this.buttons)) {
+      console.clear();
       this.init();
     }
   }
+
 }
+
 document.addEventListener("DOMContentLoaded", function () {
   new TypeFilter('.fItem', '.fType');
 });
 
-/* MINIFY VERSION WITHOUT INIT */
-class TypeFilter{init(){let a=this;document.querySelectorAll(a.buttons).forEach(b=>{b.addEventListener("click",()=>{let c=b.getAttribute("data-ftype");0<c?document.querySelectorAll(a.items+"[data-ftype]").forEach(a=>{a.getAttribute("data-ftype")==c?a.style.removeProperty("display"):a.style.display="none"}):document.querySelectorAll(a.items+"[data-ftype]").forEach(a=>{a.style.removeProperty("display")})})})}constructor(a,b){this.items=a,this.buttons=b,document.querySelector(this.items)&&document.querySelector(this.buttons)&&(console.clear(),this.init())}}
