fc154f1f532fd15f57dc5c4b7a265b89de97e570
Georgy Khudiakov <regesh@ya.ru>
2020-10-02 03:31:42 +0300

    Update typeFilter.js

--- typeFilter.js
+++ typeFilter.js
@@ -1,51 +1,110 @@
 "use strict";
-
 /*
 1. Add class "fItem" to elements to be sorted with data-ftype attribute with sort value
 2. Add class "fType" to elements to be button for selected filter with data-ftype attribute value for what to be sorted
-3. Init class as new TypeFilter('.fItem','.fType') after event DOMContentLoaded where first param is class for items and second is for switchers
+3. Init class as new TypeFilter('.fItem','.fType','activeClassName') after event DOMContentLoaded where first param is class for items and second is for switchers
 4. This classes can be changed on your wish
+5. Inner text of node element with classname fSum will be replaced with total elemens on page
+6. node element with data-ftype-total="id" where id is selector, same as on button, innerText will be replaced with total elements with placed id
 */
 class TypeFilter {
   init() {
-    let self = this;
-    document.querySelector(self.buttons).classList.add('active');
-    document.querySelectorAll(self.buttons).forEach(x => {
-      x.addEventListener('click', () => {
-        document.querySelectorAll(self.buttons).forEach(e => {
-          e.classList.remove('active');
+    this.countSum()
+    this.bindButtons()
+    this.findButtons();
+  }
+  bindButtons(){
+      self =this
+    self.buttons.forEach(x => {
+        x.addEventListener('click', (e) => {
+          let id = x.getAttribute('data-ftype');
+          if(self.id == 0){
+              x.classList.add(self.active);
+              self.id = id
+              self.hideAll()
+              self.showById(id)
+              return
+          }
+          if(self.id == id ){
+              x.classList.remove(self.active);
+              self.id = 0
+              this.showAll()
+              return
+          }
+          if(self.id != id ){
+              self.buttons.forEach((b)=>{b.classList.remove(self.active)});
+              self.id = id
+              x.classList.add(self.active);
+              self.hideAll()
+              self.showById(id)
+              return
+          }
         });
-        x.classList.add('active');
-        let type = x.getAttribute('data-ftype');
-
-        if (type > 0) {
-          document.querySelectorAll(self.items + '[data-ftype]').forEach(f => {
-            if (f.getAttribute('data-ftype') != type) {
-              f.style.display = 'none';
-            } else {
+      });
+  }
+  findButtons(){
+    let self = this  
+    let ids = []
+    self.buttons.forEach(b=>{
+        ids.push(b.getAttribute('data-ftype'))
+    })
+    ids.forEach(i => {
+        self.countById(i)
+    })
+  }
+  countById(id){
+      let total = 0;
+      this.items.forEach((i)=>{
+          if(i.getAttribute('data-ftype')==id){
+              total++
+          }
+      })
+      let sum = document.querySelector('[data-ftype-total="'+id+'"]')
+      if(sum){
+          sum.innerText = total
+      }
+      return total
+  }
+  countSum(){
+      let sum = document.querySelector('.fSum')
+      if(sum){
+          sum.innerText = this.items.length
+      }
+  }
+  hideAll(){
+    this.items.forEach(f => {
+        f.style.display = 'none';
+    })
+  }
+  showById(id){
+    this.items.forEach(f => {
+            if (f.getAttribute('data-ftype') == id) {
               f.style.removeProperty('display');
             }
           });
-        } else {
-          document.querySelectorAll(self.items + '[data-ftype]').forEach(f => {
+  }
+  showAll(){
+    this.items.forEach(f => {
             f.style.removeProperty('display');
-          });
-        }
-      });
-    });
+        });
   }
-
-  constructor(items, buttons) {
-    this.items = items;
-    this.buttons = buttons;
-
-    if (document.querySelector(this.items) && document.querySelector(this.buttons)) {
-      this.init();
+  constructor(itemsInit, buttonsInit, active) {
+        let self = this
+        document.addEventListener("DOMContentLoaded", function () {
+            let items =  (!itemsInit) ? '.fItem' : itemsInit
+            let buttons = (!buttonsInit) ? '.fType' : buttonsInit 
+            self.active = (!active) ? 'active' : active
+            self.itemsClass = items
+            self.items = document.querySelectorAll(items);  
+            self.buttons = document.querySelectorAll(buttons);
+            self.active = active
+            self.id = 0
+            if (self.items && self.buttons) {
+                self.init();
+            }
+        });
     }
-  }
-
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  new TypeFilter('.fItem', '.fType');
-});
+//Simple execute code. All params are not nessary. These are defaults
+new TypeFilter('.fType', '.fItem', 'active');
